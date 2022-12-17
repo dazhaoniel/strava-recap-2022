55c9171a429ab365654a4cec43e4b29bd3bd8961
Daniel Zhao <danielantoiny@gmail.com>
2022-12-17 02:35:52 -0600

    add parse gpx

---
+++ parse_gpx_activies.py
@@ -0,0 +1,24 @@
+import gpxpy
+import pandas as pd
+
+
+def parse_activities(file):
+	f = open('./export_11620080/'+file)
+	g = gpxpy.parse(f)
+	data = g.tracks[0].segments[0].points
+	return data
+
+def get_activity_coordinates():
+	df = pd.DataFrame(columns=['latitude', 'longitude', 'elevation', 'activity_name'])
+	a = pd.read_pickle('./activities.pkl')
+	for r in list(a.itertuples(index=False, name=None)):
+		n = str(r[1])
+		d = parse_activities(n)
+		for p in d:
+			df.loc[len(df.index)] = [p.latitude, p.longitude, p.elevation, str(r[0])]
+
+	df.to_pickle('activity_location_data.pkl')
+
+
+if __name__ == '__main__':
+	get_activity_coordinates()
