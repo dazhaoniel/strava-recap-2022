3b8f7dfae86b04157a9d1d3b2db92b6241d9ae76
Daniel Zhao <danielantoiny@gmail.com>
2022-12-18 11:39:50 -0600

    add functions

--- parse_gpx_activies.py
+++ parse_gpx_activies.py
@@ -11,13 +11,14 @@ def parse_activities(file):
 def get_activity_coordinates():
 	df = pd.DataFrame(columns=['latitude', 'longitude', 'elevation', 'activity_name'])
 	a = pd.read_pickle('./activities.pkl')
+	# print(len(list(a.itertuples(index=False, name=None))))
 	for r in list(a.itertuples(index=False, name=None)):
 		n = str(r[1])
 		d = parse_activities(n)
 		for p in d:
 			df.loc[len(df.index)] = [p.latitude, p.longitude, p.elevation, str(r[0])]
 
-	df.to_pickle('activity_location_data.pkl')
+		df.to_pickle('./activities/activity_location_data_'+str(r[0])+'.pkl')
 
 
 if __name__ == '__main__':
