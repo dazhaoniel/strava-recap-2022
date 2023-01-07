f7e7690aee087bc1ff7fb477676d81ebe05253bc
Daniel Zhao <danielantoiny@gmail.com>
2023-01-07 01:05:17 -0600

    try to load csv

--- react-strava-visualization/src/App.js
+++ react-strava-visualization/src/App.js
@@ -2,12 +2,12 @@ import "./App.css";
 import LineChart from "./Charts/LineChart";
 import Histogram from "./Charts/Histogram";
 import TimeSeries from "./Charts/TimeSeries";
-import data from "../..export_11620080/activities.csv";
+import data from "./daniel-activities.csv";
 
 function App() {
   return (
     <div className="row">
-      <LineChart height={400} width={400} />
+      <LineChart height={400} width={400} csv={data} />
     </div>
   );
 }
